5e1ceeab53c057a73f25313f8cb3437855dfd5fd
MuidKhan-sudo <muidurrashid@gmail.com>
2026-04-17 15:47:58 +0600

    Adding and designing navbar and its functionalities

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './layout/root.jsx';
+import Home from './pages/home/Home.jsx';
+import FriendDetails from './pages/FriendDetails/FriendDetails.jsx';
+import Timeline from './pages/Timeline/Timeline.jsx';
+import Stats from './pages/Stats/Stats.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children:[
+      {path:"/",
+        index:true,
+        Component: Home,
+      },
+      {
+        path:"/friendDetails",
+        Component:FriendDetails
+      },
+      {
+        path:"/timeline",
+        Component:Timeline
+      },
+      {
+        path:"/stats",
+        Component:Stats
+      }
+
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />,
+  </StrictMode>,
+)
